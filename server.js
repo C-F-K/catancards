84ada8c9f42abceb423e56aa87e255afc73666ba
Cameron Finn Kelly <a.fell.ninny.mocker@gmail.com>
2015-03-10 08:46:24 +0000

    port tho

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-var port = process.env.PORT || 3000;
-app.listen(port);
+var port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0");
 console.log("Server is listening on port " + port);
