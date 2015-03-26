b60345d028bc7679cfaa50f63288eb572842ca34
Cameron Finn Kelly <a.fell.ninny.mocker@gmail.com>
2015-03-26 12:11:35 +0000

    pages tho

--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/public'));
-var port = process.env.PORT || 8080;
-app.listen(port, "0.0.0.0");
-console.log("Server is listening on port " + port);
